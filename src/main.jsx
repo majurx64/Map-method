8d907bd5d7e82b0e9adf453d1fb753ad9fdf083c
Mansur <mansurchik982@gmail.com>
2026-09-16 02:57:19 +0300

    Redirect Vercel alias to canonical domain

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+if (window.location.hostname === 'map-method-chi.vercel.app') {
+  window.location.replace(
+    `https://www.mapmethod.ru${window.location.pathname}${window.location.search}${window.location.hash}`,
+  )
+}
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
