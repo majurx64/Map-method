cda0bea072856536494485f1ae7084fdbbf372e7
majurx64 <mansurchik982@gmail.com>
2026-09-16 02:46:17 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
+  base: 'https://map-method-chi.vercel.app/',
   plugins: [react()],
   build: {
     rolldownOptions: {
